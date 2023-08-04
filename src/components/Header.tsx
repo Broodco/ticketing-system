c07f2ebbad2ef7e7b6a7cf625516f0a629458f18
Broodco <broodcoorens.maxime@gmail.com>
2023-08-04 10:46:41 +0200

    ADD: OAuth with Github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 export default function Header() {
   const { data: session, status } = useSession();
   const currentPathname = usePathname();
