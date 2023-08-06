9a444ffea45cb809a68065521adbeed5a484332f
Broodco <broodcoorens.maxime@gmail.com>
2023-08-06 15:21:51 +0200

    DEL pages auth route ADD app auth route

--- src/pages/api/auth/[...nextauth].ts
+++
@@ -1,6 +0,0 @@
-import {NextApiHandler} from "next";
-import NextAuth from "next-auth";
-import {authOptions} from "@/lib/auth";
-
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
-export default authHandler;
